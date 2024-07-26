ff26e321964794006142fe18293c0a6acf17c0c6
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-07-26 11:55:25 +0900

    user 삭제

--- Mysql-Gradle/src/main/java/com/kh/controller/UserController.java
+++ Mysql-Gradle/src/main/java/com/kh/controller/UserController.java
@@ -3,10 +3,12 @@ package com.kh.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.dto.User;
@@ -28,4 +30,17 @@ public class UserController {
 	public void insertUser(@RequestBody User user) {
 		userService.insertUser(user);
 	}
+	// RequestBody 는 전체를 
+	// RequestParam 은 일부를
+	@DeleteMapping // 삭제를 진행하기 위해 만나는 주소(API) users/유저번호
+	public void deleteUser(@RequestParam("id") int id) {
+		userService.deleteUser(id);
+	}
+	
+	// RequestBody 는 전체를 
+	// RequestParam 은 일부를
+	//@DeleteMapping("/{id}") // 삭제를 진핼하기 위해 만나는 주소 user
+	//public void deleteUser(@PathVariable int id) {
+	//	userService.deleteUser(id);
+	//}
 }
