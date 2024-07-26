9952e65f868d764b9aabd3d775c586a7684a1948
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-07-26 14:28:29 +0900

    수정 삭제

--- Mysql-Gradle/src/main/java/com/kh/controller/UserController.java
+++ Mysql-Gradle/src/main/java/com/kh/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,4 +44,9 @@ public class UserController {
 	//public void deleteUser(@PathVariable int id) {
 	//	userService.deleteUser(id);
 	//}
+	
+	@PutMapping  // 수정
+	public void updateUser(@RequestBody User user) {
+		userService.updateUser(user);
+	}
 }
