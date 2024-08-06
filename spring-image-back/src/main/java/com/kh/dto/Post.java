8aa40fbeaba66bdcc5af55d16aec687499a09c74
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-08-06 11:03:33 +0900

    다중 이미지 DB 저장

--- spring-image-back/src/main/java/com/kh/dto/Post.java
+++ spring-image-back/src/main/java/com/kh/dto/Post.java
@@ -13,5 +13,8 @@ public class Post {
 	private int id;
 	private String title;
 	private String content;
-	private List<Image> images; 
+	private String imageUrl;
+	// id 와 createdAt 같은 경우 MySQL 에서 자동으로 숫자와 날짜를 생성해주기 때문에
+	// mapper.xml 에 작성하지 않음
+	private String createdAt; // 게시판에 작성한 글과 이미지가 MySQL 에 들어온 시간
 }
