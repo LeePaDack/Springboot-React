58ac410817d850ddf4be22752cab63b7981756df
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-08-22 11:46:32 +0900

    경로 수정

--- chicken-store/src/App.js
+++ chicken-store/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ChickenList from './component/ChickenList';
 import ChickenDetail from './component/ChickenDetail';
+import MainRouter from './MainRouter';
 /*
 ChickenList path="/"
 
@@ -13,8 +14,10 @@ function App () {
 
     return (
         <Router>
+            
             <Routes>
-                <Route path='/' element={<ChickenList />}/> 
+                <Route path='/' element={<MainRouter/>}/> 
+                {/* Routes 안에는 Route 로 설정된 태그만 들어올 수 있음  <MainRouter/> */}
                 <Route path='/chicken-detail/:id' element={<ChickenDetail />}/> 
             </Routes>
         </Router>
