fbb6cc9e937ab2ca43df57d048e6b78ff778d2d9
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-08-23 11:43:12 +0900

    JPA 검색 관련 기능 추가

--- JPA_chicken/src/main/java/com/kh/repository/ChickenRepository.java
+++ JPA_chicken/src/main/java/com/kh/repository/ChickenRepository.java
@@ -1,5 +1,7 @@
 package com.kh.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,17 @@ import com.kh.dto.Chicken;
 // @Repository, @Mapper 는 interface 로 시작
 @Repository
 public interface ChickenRepository extends JpaRepository<Chicken, Integer> {
+	// 검색은 sql 문이 예외적이기 때문에 필수로 작성
+	/*
+	findByChickenName : 어떤 컬럼에서 검색   where chickenName = "?"
+	
+	Containing  : 부분 일치 허용   Like %...% 해당
+	
+	IgnoreCase  : 대소문자 구분 없이 검색   사용같음
+	 */
+	List<Chicken> findByChickenNameContainingIgnoreCase(String query);
+	
+	
 	// 전체보기 전체넣기 전체수정 전체삭제 와 같은 기본 기능은
 	// JpaRepository 안에 모두 들어있음
 	
