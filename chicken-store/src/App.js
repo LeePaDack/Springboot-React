5b5be06144f892fac1b3160f772669e5e99ef397
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-08-26 13:35:29 +0900

    경로 설정

--- chicken-store/src/App.js
+++ chicken-store/src/App.js
@@ -15,7 +15,7 @@ function App () {
 
     return (
         <Router>
-            
+                
             <Routes>
                 <Route path='/' element={<MainRouter/>}/> 
                 {/* Routes 안에는 Route 로 설정된 태그만 들어올 수 있음  <MainRouter/> */}
